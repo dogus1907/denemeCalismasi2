844971f81a8b89cc0f1d5cd804c89836365e7276
dogus1907 <doguskoca5@gmail.com>
2023-06-08 21:24:46 +0300

    Bu dosya sorunsuz bir şekilde çalışmaktadır

---
+++ app/src/main/java/com/doguskoca/denemecalismasi2/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.doguskoca.denemecalismasi2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    // Proje ile ilgili denemeler yapılmıştır.
+
+    // Proje sunusu kontrol edilmektedir.
+
+    // Denemeler devam ediyor
+}
